abef8642a3d22ae9a74ceaf7a7d8f900a9c3b689
WeiZ-boot <74607045+WeiZ-boot@users.noreply.github.com>
2024-03-13 23:56:34 +0800

    Update toolFunc.py

--- apps/toolsapi/toolFunc.py
+++ apps/toolsapi/toolFunc.py
@@ -871,7 +871,7 @@ def analyzeContracts_ccanalyzer():
         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 text=True)
         # If the command was successful, return the output
-        return jsonify({"message": "Analysis completed", "output": command}), 200
+        return jsonify({"message": "Analysis completed", "output": result}), 200
     except subprocess.CalledProcessError as e:
         # If an error occurred while executing the command, return the error
         return jsonify({"error": "Analysis failed", "message": e.stderr}), 500
