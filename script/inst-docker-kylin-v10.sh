341e8192e593feae8a371260ce7a7808c430d159
esenle <EiddleChen1008@gmail.com>
2024-10-12 13:20:47 +0000

    wip

--- script/inst-docker-kylin-v10.sh
+++ script/inst-docker-kylin-v10.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# 银河麒麟安装正确版本的 docker 和 docker-compse 
+
 sudo yum install -y docker
+
+# Install Docker Compose from a China mirror
+DOCKER_COMPOSE_VERSION="1.29.2" # Change this to the desired version
+sudo curl -L https://mirrors.aliyun.com/docker/compose/$(uname -s)/$(uname -m)/docker-compose -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 echo '#######################################################################'
 echo '# cat /etc/docker/daemon.json'
