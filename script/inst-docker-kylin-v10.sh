5bd9713d2538afb203043dd55a556ae7e19385e9
esenle <EiddleChen1008@gmail.com>
2024-10-12 14:03:41 +0000

    wip

--- script/inst-docker-kylin-v10.sh
+++ script/inst-docker-kylin-v10.sh
@@ -2,31 +2,13 @@
 
 # 银河麒麟安装正确版本的 docker 和 docker-compse 
 
-# sudo yum install -y docker
-# 卸载旧版本的 Docker（如果存在）
-sudo dnf remove -y docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
+sudo yum install -y docker
 
 # Install Docker Compose from a China mirror
-DOCKER_VERSION="20.10.21"
-DOCKER_COMPOSE_VERSION="2.12.2"
-CONTAINERD_VERSION="1.6.21"
-rm -rf /tmp/docker-rpm-for-kylin
-mkdir -p /tmp/docker-rpm-for-kylin
-cd /tmp/docker-rpm-for-kylin
-# wget https://mirrors.aliyun.com/docker-ce/linux/centos/8/x86_64/stable/Packages/containerd.io-${CONTAINERD_VERSION}-3.1.el8.x86_64.rpm
-# wget https://mirrors.aliyun.com/docker-ce/linux/centos/8/x86_64/stable/Packages/docker-ce-cli-${DOCKER_VERSION}-3.el8.x86_64.rpm
-wget https://mirrors.aliyun.com/docker-ce/linux/centos/8/x86_64/stable/Packages/docker-ce-${DOCKER_VERSION}-3.el8.x86_64.rpm
-wget https://mirrors.aliyun.com/docker-ce/linux/centos/8/x86_64/stable/Packages/docker-compose-plugin-${DOCKER_COMPOSE_VERSION}-3.el8.x86_64.rpm
-sudo dnf install -y ./*.rpm
-docker compose version
-cd -
+DOCKER_COMPOSE_VERSION="1.20.0" # Change this to the desired version
+sudo curl -L "https://mirrors.aliyun.com/docker-toolbox/linux/compose/$DOCKER_COMPOSE_VERSION/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 echo '#######################################################################'
 echo '# cat /etc/docker/daemon.json'
