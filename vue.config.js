fc6d367f96f5c0678f5049db77643b345a7169c3
esenle <EiddleChen1008@gmail.com>
2024-10-24 16:20:45 +0800

    wip

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/static/cms/'
-    : '/',
-  devServer: {
-    proxy: 'http://localhost:5000'
-  }
-}
